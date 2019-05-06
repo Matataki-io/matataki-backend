4283d335fa6be8ec198241cb5b95cabc17edea8f
Joe <343747757@qq.com>
2019-05-06 16:19:20 +0800

    ont auth

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -5,34 +5,41 @@ const moment = require('moment');
 const ecc = require('eosjs-ecc');
 const base64url = require('base64url');
 const jwt = require('jwt-simple');
+let ONT = require('ontology-ts-sdk');
+
 
 class AuthController extends Controller {
 
   async auth() {
-    const ctx = this.ctx;
 
     // 1. 取出签名
-    const { username, publickey, sign } = ctx.request.body;
-    console.log(username, publickey, sign);
+    const { username, publickey, sign, platform = 'eos'} = this.ctx.request.body;
 
+    if ('eos' === platform) {
+      this.eos_auth(sign, username, publickey);
+    } else if ('ont' === platform) {
+      this.ont_auth(sign, username, publickey);
+    } else {
+      this.ctx.status = 401;
+      this.ctx.body = 'platform not support';
+    }
+
+  }
+
+  eos_auth(sign, username, publickey) {
     // 2. 验证签名
     try {
       const recover = ecc.recover(sign, username);
 
       if (recover !== publickey) {
-        ctx.body = {
-          msg: 'invalid signature',
-        };
-        ctx.status = 500;
-
+        this.ctx.body = { msg: 'invalid signature' };
+        this.ctx.status = 500;
         return;
       }
+
     } catch (err) {
-      ctx.logger.error(err.sqlMessage);
-      ctx.body = {
-        msg: 'invalid signature ' + err,
-      };
-      ctx.status = 500;
+      this.ctx.body = { msg: 'invalid signature ' + err };
+      this.ctx.status = 500;
       return;
     }
 
@@ -44,8 +51,41 @@ class AuthController extends Controller {
       exp: expires,
     }, this.app.config.jwtTokenSecret);
 
-    ctx.body = token
+    this.ctx.body = token;
   }
+
+  ont_auth(sign, username, publickey) {
+
+    const pub = new ONT.Crypto.PublicKey(publickey);
+
+    const msg = ONT.utils.str2hexstr(username);
+
+    const signature = ONT.Crypto.Signature.deserializeHex(sign);
+
+    const pass = pub.verify(msg, signature);
+
+    if (pass) {
+
+      // 3. 签名有效，生成accessToken . accessToken = username + date + secret (JWT format)
+      var expires = moment().add(7, "days").valueOf();
+
+      var token = jwt.encode({
+        iss: username,
+        exp: expires,
+      }, this.app.config.jwtTokenSecret);
+
+      this.ctx.body = token;
+
+    } else {
+      this.ctx.body = { msg: 'invalid signature' };
+      this.ctx.status = 500;
+    }
+
+    // curl -d "platform=ont&publickey=0205c8fff4b1d21f4b2ec3b48cf88004e38402933d7e914b2a0eda0de15e73ba61&username=helloworld&sign=010936f0693e83d5d605816ceeeb4872d8a343d4c7350ef23e49614e0302d94d6f6a4af73e20ed9c818c0be6865e6096efc7b9f98fa42a33f775ff0ea1cb17703a" -H "Authorization: Basic bXlfYXBwOm15X3NlY3JldA==" -v -X POST http://127.0.0.1:7001/auth
+
+
+  }
+
 }
 
 module.exports = AuthController;
