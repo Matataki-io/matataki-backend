f83a7e01bd1a754f342d1fbe9fe562daf9223876
Joe <343747757@qq.com>
2019-05-13 20:45:46 +0800

    移除无用code

--- app/controller/share.js
+++ app/controller/share.js
@@ -6,34 +6,6 @@ var _ = require('lodash');
 
 class ShareController extends Controller {
 
-  async share() {
-    const ctx = this.ctx;
-
-    const { user = '', hash = '' } = ctx.request.body;
-
-    try {
-      const result = await this.app.mysql.insert('shares', {
-        username: user,
-        hash,
-        create_time: this.app.mysql.literals.now,
-      });
-
-      const updateSuccess = result.affectedRows === 1;
-
-      if (updateSuccess) {
-        ctx.status = 200;
-      } else {
-        ctx.status = 500;
-      }
-    } catch (err) {
-      ctx.logger.error(err.sqlMessage);
-      ctx.body = {
-        msg: 'insert error' + err.sqlMessage,
-      };
-      ctx.status = 500;
-    }
-  }
-
 
   async shares() {
     const pagesize = 20;
