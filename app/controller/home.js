96442da0c75364cd268e2fedf6f6b8de2bd46902
Joe <343747757@qq.com>
2019-05-14 23:13:56 +0800

    version update

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg, version=1.3';
+    this.ctx.body = 'hi, egg, version=1.4';
   }
 }
 
