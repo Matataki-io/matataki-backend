0056035a8709ab1eaa2202d75810af70931d50b5
Kumo <gdut7890@outlook.com>
2019-05-21 14:21:39 +0800

    优化SQL查询格式

--- app/service/share.js
+++ app/service/share.js
@@ -24,7 +24,8 @@ class ShareService extends Service {
     const results = await this.app.mysql.query(
       'SELECT s.amount, s.platform, s.signid, s.create_time, u.username, u.nickname, u.avatar, c.comment FROM supports s '
       // 一个user在同一篇文章下的comment和support
-      + 'INNER JOIN users u INNER JOIN comments c ON c.sign_id = s.signid AND c.username = u.username AND s.uid = u.id '
+      + 'INNER JOIN users u ON s.uid = u.id '
+      + 'INNER JOIN comments c ON c.sign_id = s.signid AND c.username = u.username '
       + 'WHERE s.status = 1 AND s.signid = :signid ORDER BY s.create_time DESC limit :start, :end;',
       { signid, start: (page - 1) * pagesize, end: pagesize }
     );
