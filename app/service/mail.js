e7f1472958e47c39463ec4b1cfcf5a139b49b677
Kumo <gdut7890@outlook.com>
2019-05-24 13:00:52 +0800

    优化邮件模块格式和邮件模板

--- app/service/mail.js
+++ app/service/mail.js
@@ -6,35 +6,40 @@ class MailService extends Service {
 
   async sendMail(supportid) {
 
-    // 获取赞赏信息
-    const support = await this.app.mysql.get('supports', { id: supportid });
-    if (support === null) {
-      return null;
-    }
-    // 获取库存信息
-    const stock = await this.app.mysql.get('product_stocks', { support_id: support.id, status: 0 });
-    if (stock === null) {
-      return null;
-    }
-    // 获取用户信息
-    const user = await this.app.mysql.get('users', { id: support.uid });
-    if (user === null) {
-      return null;
-    }
-
-    const mailContent = '<h2>Your Steam Key</h2>'
-      + `<p>[ ${stock.digital_copy} ] -by Smart Signature Project</p>`;
-    const mailOptions = {
-    //   from: this.config.mail.auth.user,
-      from: 'Andoromeda Official',
-      to: user.email,
-      subject: 'Your Steam Key',
-      html: mailContent,
+    this.app.mysql.queryFromat = function(query, values) {
+      if (!values) return query;
+      return query.replace(/\:(\w+)/g, function(txt, key) {
+        if (values.hasOwnProperty(key)) {
+          return this.escape(values[key]);
+        }
+        return txt;
+      }.bind(this));
     };
-    let result = null;
 
+    const product = await this.app.mysql.query(
+      'SELECT u.username, u.email FROM supports s INNER JOIN users u ON s.uid = u.id WHERE s.id = :supportid;'
+      + 'SELECT s.digital_copy, p.title FROM product_stocks s INNER JOIN product_prices p ON s.sign_id = p.sign_id WHERE s.support_id = :supportid LIMIT 1;',
+      { supportid }
+    );
+    const user = product[0];
+    const stock = product[1];
+    if (user.length === 0 || stock.length === 0) {
+      return null;
+    }
+
+    let result = null;
     try {
       // 配置以及发送邮件
+      const mailData = { user: user[0].username, stockName: stock[0].title, key: stock[0].digital_copy };
+      const mailContent = await this.ctx.renderView('mail.tpl', mailData, { viewEngine: 'nunjucks' });
+      const mailOptions = {
+      //   from: this.config.mail.auth.user,
+        from: `Andoromeda Official <${this.config.mail.auth.user}>`,
+        to: user[0].email,
+        subject: '智能签名:您购买的商品',
+        html: mailContent,
+      };
+
       const transpoter = await nodemailer.createTransport(this.config.mail);
       result = await transpoter.sendMail(mailOptions);
     } catch (err) {
--- config/plugin.js
+++ config/plugin.js
@@ -10,4 +10,8 @@ module.exports = {
     enable: true,
     package: 'egg-cors',
   },
+  nunjucks: {
+    enable: true,
+    package: 'egg-view-nunjucks',
+  },
 };
