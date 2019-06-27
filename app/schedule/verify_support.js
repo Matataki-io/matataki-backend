e7937c97278afa6860d3f00b97f9006852d502cd
cnchenhao <6644596@qq.com>
2019-06-27 18:42:08 +0800

    调试关闭定时任务

--- app/schedule/verify_support.js
+++ app/schedule/verify_support.js
@@ -26,6 +26,7 @@ class VerifySupport extends Subscription {
   }
 
   async subscribe() {
+    return;
     // if (this.ctx.app.config.isDebug) return;
 
     const expire = moment().subtract(1, 'hours').format('YYYY-MM-DD HH:mm:ss');
