2a234712bf732989f88f1f00c089b17a4229aba1
cnchenhao <6644596@qq.com>
2019-06-29 23:39:42 +0800

    修改/tokens、/balance资产类型

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,7 +2,7 @@
 
 const Controller = require('../core/base_controller');
 const moment = require('moment');
-var _ = require('lodash');
+const _ = require('lodash');
 const ONT = require('ontology-ts-sdk');
 class UserController extends Controller {
 
@@ -23,10 +23,10 @@ class UserController extends Controller {
   }
 
   async tokens() {
-    const { page = 1, symbol = "EOS" } = this.ctx.query;
-    let pagesize = 20;
+    const { page = 1, symbol = 'EOS' } = this.ctx.query;
+    const pagesize = 20;
 
-    // 1. 历史总创作收入 (sign income)
+    // 1. 历史总创作收入
     const tokens = await this.app.mysql.query(
       'select id, contract, symbol, amount, platform from assets where uid = ? and symbol= ? ',
       [ this.ctx.user.id, symbol ]
@@ -37,33 +37,51 @@ class UserController extends Controller {
       [ this.ctx.user.id, symbol, (page - 1) * pagesize, pagesize ]
     );
 
-    let totalSignIncome = await this.app.mysql.query(
+    // 作者收入
+    const authorSaleIncome = await this.app.mysql.query(
       'select sum(amount) as totalSignIncome from assets_change_log where type = ? and uid = ? and symbol = ?',
-      [ "sign income", this.ctx.user.id, symbol ]
+      [ 'author_sale_income', this.ctx.user.id, symbol ]
     );
+    const authorSupportedIncome = await this.app.mysql.query(
+      'select sum(amount) as totalSignIncome from assets_change_log where type = ? and uid = ? and symbol = ?',
+      [ 'author_supported_income', this.ctx.user.id, symbol ]
+    );
+    const totalSignIncome = authorSaleIncome + authorSupportedIncome;
 
-    let totalShareIncome = await this.app.mysql.query(
+    // 分享者收入
+    const fissionIncome = await this.app.mysql.query(
       'select sum(amount) as totalShareIncome from assets_change_log where type = ? and uid = ? and symbol = ?',
-      [ "share income", this.ctx.user.id, symbol ]
+      [ 'fission_income', this.ctx.user.id, symbol ]
     );
+    const referralIncome = await this.app.mysql.query(
+      'select sum(amount) as totalShareIncome from assets_change_log where type = ? and uid = ? and symbol = ?',
+      [ 'referral_income', this.ctx.user.id, symbol ]
+    );
+    const totalShareIncome = fissionIncome + referralIncome;
 
-    let totalShareExpenses = await this.app.mysql.query(
+    // 投资/购买支出
+    const supportExpenses = await this.app.mysql.query(
       'select sum(amount) as totalShareExpenses from assets_change_log where type = ? and uid = ? and symbol = ?',
-      [ "support expenses", this.ctx.user.id, symbol ]
+      [ 'support_expenses', this.ctx.user.id, symbol ]
     );
+    const buyExpenses = await this.app.mysql.query(
+      'select sum(amount) as totalShareExpenses from assets_change_log where type = ? and uid = ? and symbol = ?',
+      [ 'buy_expenses', this.ctx.user.id, symbol ]
+    );
+    const totalShareExpenses = supportExpenses + buyExpenses;
 
     let balance = 0;
     if (tokens && tokens.length > 0) {
       balance = tokens[0].amount;
     }
 
-    let result = {
-      balance: balance,                                                   // 余额（待提现）
-      totalSignIncome: totalSignIncome[0].totalSignIncome || 0,           // 总创作收入
-      totalShareIncome: totalShareIncome[0].totalShareIncome || 0,        // 总打赏收入 
-      totalShareExpenses: totalShareExpenses[0].totalShareExpenses || 0,  // 总打赏支出
-      logs: logs                                                          // 流水（之后再来处理分页）
-    }
+    const result = {
+      balance, // 余额（待提现）
+      totalSignIncome: totalSignIncome[0].totalSignIncome || 0, // 总创作收入
+      totalShareIncome: totalShareIncome[0].totalShareIncome || 0, // 总打赏收入
+      totalShareExpenses: totalShareExpenses[0].totalShareExpenses || 0, // 总打赏支出
+      logs, // 流水（之后再来处理分页）
+    };
 
     this.ctx.body = this.ctx.msg.success;
     this.ctx.body.data = result;
@@ -77,10 +95,10 @@ class UserController extends Controller {
     );
 
     for (let i = 0; i < tokens.length; i++) {
-      let token = tokens[i];
-      let value = await this.app.mysql.query(
+      const token = tokens[i];
+      const value = await this.app.mysql.query(
         'select sum(amount) as value from assets_change_log where uid = ? and symbol = ? and type in (?)',
-        [ this.ctx.user.id, token.symbol, [ "sign income", "share income" ]]
+        [ this.ctx.user.id, token.symbol, [ 'fission_income', 'referral_income', 'author_sale_income', 'author_supported_income' ]]
       );
 
       tokens[i].totalIncome = value[0].value || 0;
@@ -105,7 +123,7 @@ class UserController extends Controller {
       // 如果ID不存在, 会以此ID创建一条新的用户数据, 不过因为jwt secret不会被知道, 所以对外不会发生
       const result = await this.app.mysql.query(
         'INSERT INTO users (id, avatar, create_time) VALUES ( ?, ?, ?) ON DUPLICATE KEY UPDATE avatar = ?',
-        [userid, avatar, now, avatar]
+        [ userid, avatar, now, avatar ]
       );
 
       const updateSuccess = result.affectedRows >= 1;
@@ -180,96 +198,96 @@ class UserController extends Controller {
 
     // 签名验证
     try {
-      if ('eos' === ctx.user.platform) {
+      if (ctx.user.platform === 'eos') {
         // EOS最小提现 (测试先不限制)
         // if(amount < 10000){
         //   return this.response(403, "EOS withdtaw amount must greater than 1 ");
         // }
 
         let sign_data = `${toaddress} ${contract} ${symbol} ${amount}`;
-        if ("ont" === platform) {
+        if (platform === 'ont') {
           sign_data = `${toaddress.slice(0, 12)} ${toaddress.slice(12, 24)} ${toaddress.slice(24, 36)} ${contract.slice(0, 12)} ${contract.slice(12, 24)} ${contract.slice(24, 36)} ${symbol} ${amount}`;
         }
-        console.log("debug for withdraw", ctx.user.platform, sign_data, publickey, sign);
+        console.log('debug for withdraw', ctx.user.platform, sign_data, publickey, sign);
         await this.eos_signature_verify(ctx.user.username, sign_data, sign, publickey);
-      } else if ('ont' === ctx.user.platform) {
+      } else if (ctx.user.platform === 'ont') {
         // ONT最小提现 (测试先不限制)
         // if(amount < 30000){
         //   return this.response(403, "ONT withdtaw amount must greater than 3 ONT");
         // }
 
-        let sign_data = `${toaddress} ${contract} ${symbol} ${amount}`;
+        const sign_data = `${toaddress} ${contract} ${symbol} ${amount}`;
         const msg = ONT.utils.str2hexstr(sign_data);
         await this.ont_signature_verify(msg, sign, publickey, publickey, sign);
       } else if (ctx.user.platform === 'github') {
         this.logger.info('UserController:: withdraw: There is a github user withdrawing...');
       } else {
-        ctx.body = ctx.msg.postPublishSignVerifyError;  //'platform not support';
+        ctx.body = ctx.msg.postPublishSignVerifyError; // 'platform not support';
         return;
       }
     } catch (err) {
-      console.log("signature_verify error", err);
-      ctx.body = ctx.msg.postPublishSignVerifyError;  //err.message;
+      console.log('signature_verify error', err);
+      ctx.body = ctx.msg.postPublishSignVerifyError; // err.message;
       return;
     }
 
-    let asset = await this.app.mysql.get('assets', { uid: ctx.user.id, symbol: symbol, platform: platform, contract: contract });
+    let asset = await this.app.mysql.get('assets', { uid: ctx.user.id, symbol, platform, contract });
 
     if (!asset) {
-      return this.response(403, "not available asset can withdtaw");
+      return this.response(403, 'not available asset can withdtaw');
     }
 
-    let withdraw_amount = parseInt(amount);
+    const withdraw_amount = parseInt(amount);
 
     if (!withdraw_amount) {
-      return this.response(403, "invalid amount");
+      return this.response(403, 'invalid amount');
     }
 
     if (withdraw_amount <= 0) {
-      return this.response(403, "invalid amount");
+      return this.response(403, 'invalid amount');
     }
 
     if (!toaddress) {
-      return this.response(403, "withdraw address required");
+      return this.response(403, 'withdraw address required');
     }
 
-    if ('ont' === platform) {
-      let num = withdraw_amount / 10000;
+    if (platform === 'ont') {
+      const num = withdraw_amount / 10000;
       if (!this.isInteger(num)) {
-        return this.response(403, "ONT withdraw only support Integer");
+        return this.response(403, 'ONT withdraw only support Integer');
       }
     }
 
-    let transfer_memo = memo ? memo : "Withdraw from Smart Signature";
+    const transfer_memo = memo ? memo : 'Withdraw from Smart Signature';
 
     try {
       const conn = await this.app.mysql.beginTransaction();
 
       try {
         // for update 锁定table row
-        let result = await conn.query('SELECT * FROM assets WHERE id=? limit 1 FOR UPDATE;', [asset.id]);
+        const result = await conn.query('SELECT * FROM assets WHERE id=? limit 1 FOR UPDATE;', [ asset.id ]);
 
         asset = result[0];
 
         if (withdraw_amount > asset.amount) {
-          throw new Error("withdraw amount should less than balance");
+          throw new Error('withdraw amount should less than balance');
         }
 
-        let remind_amount = asset.amount - withdraw_amount;
+        const remind_amount = asset.amount - withdraw_amount;
 
-        await conn.update("assets", {
-          amount: remind_amount
+        await conn.update('assets', {
+          amount: remind_amount,
         }, { where: { id: asset.id } });
 
         const now = moment().format('YYYY-MM-DD HH:mm:ss');
-        await conn.insert("assets_change_log", {
+        await conn.insert('assets_change_log', {
           uid: ctx.user.id,
-          contract: contract,
-          symbol: symbol,
+          contract,
+          symbol,
           amount: withdraw_amount,
-          platform: platform,
-          type: "withdraw",
-          toaddress: toaddress,
+          platform,
+          type: 'withdraw',
+          toaddress,
           memo: transfer_memo,
           status: 0,
           create_time: now,
@@ -285,28 +303,28 @@ class UserController extends Controller {
 
     } catch (err) {
       ctx.logger.error(err.sqlMessage);
-      this.response(500, 'withdraw error ' + err.sqlMessage)
+      this.response(500, 'withdraw error ' + err.sqlMessage);
     }
   }
 
   async search() {
-    const { q = "" } = this.ctx.query;
+    const { q = '' } = this.ctx.query;
 
-    let user = await this.app.mysql.get("users", { nickname: q });
+    let user = await this.app.mysql.get('users', { nickname: q });
     if (!user) {
-      user = await this.app.mysql.get("users", { username: q });
+      user = await this.app.mysql.get('users', { username: q });
     }
-    
+
     if (!user) {
       this.ctx.body = ctx.msg.userNotExist;
       return;
     }
 
-    let result = {
+    const result = {
       id: user.id,
-      avatar: user.avatar || "",
+      avatar: user.avatar || '',
       nickname: user.nickname,
-      username: user.username
+      username: user.username,
     };
 
     this.ctx.body = this.ctx.msg.success;
