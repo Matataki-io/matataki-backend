f709f5c8960926c7c70b75f8ba8493aea3de7519
zxplus <shellteo@163.com>
2019-09-24 17:18:30 +0800

    🐛 bugfix:修改page整型的bug

--- app/controller/token.js
+++ app/controller/token.js
@@ -5,11 +5,11 @@ const Controller = require('../core/base_controller');
 class TokenController extends Controller {
   async tokenList() {
     const ctx = this.ctx;
-    const { pagesize = 20, page = 1 } = this.ctx.query;
+    const { pagesize = 10, page = 1 } = this.ctx.query;
     // 用户id
     const user_id = ctx.user.id;
     // token list
-    const result = await ctx.service.exchange.getTokenListByUser(user_id, page, pagesize);
+    const result = await ctx.service.exchange.getTokenListByUser(user_id, parseInt(page), parseInt(pagesize));
     ctx.body = {
       ...ctx.msg.success,
       data: result,
@@ -18,14 +18,14 @@ class TokenController extends Controller {
 
   async userList() {
     const ctx = this.ctx;
-    const { pagesize = 20, page = 1 } = this.ctx.query;
+    const { pagesize = 10, page = 1 } = this.ctx.query;
     // user id
     const user_id = ctx.user.id;
     // 根据user_id查找用户发行的token
     const token = await ctx.service.token.mineToken.getByUserId(user_id);
     const token_id = token.id;
     // token list
-    const result = await ctx.service.exchange.getUserListByToken(token_id, page, pagesize);
+    const result = await ctx.service.exchange.getUserListByToken(token_id, parseInt(page), parseInt(pagesize));
     ctx.body = {
       ...ctx.msg.success,
       data: result,
