ec9ba8af61c749ae6414ec552d94a6bf5af5605c
cnchenhao <6644596@qq.com>
2019-10-14 15:18:43 +0800

    文章list返回require_holdtokens

--- app/service/post.js
+++ app/service/post.js
@@ -720,7 +720,7 @@ class PostService extends Service {
       }
     }
 
-    sqlcode += ' a.hash, a.create_time, a.cover, b.nickname, b.avatar FROM posts a';
+    sqlcode += ' a.hash, a.create_time, a.cover, a.require_holdtokens, b.nickname, b.avatar FROM posts a';
     sqlcode += ' LEFT JOIN users b ON a.uid = b.id WHERE a.id IN (?) AND a.status = 0 ORDER BY id DESC;';
     postList = await this.app.mysql.query(
       sqlcode,
