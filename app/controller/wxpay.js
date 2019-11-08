36028d6e1539ac1f470dd044b22346d954bab853
shellteo <shellteo@163.com>
2019-11-08 18:13:29 +0800

    split wx/pay to createOrder and pay

--- app/controller/wxpay.js
+++ app/controller/wxpay.js
@@ -11,6 +11,116 @@ const typeOptions = {
 };
 
 class WxPayController extends Controller {
+  async createOrder() {
+    const { ctx } = this;
+    // total: 输入的cny数值，单位元
+    // type类型见typeOptions：add，buy_token，sale_token
+    // token_amount: 输出的token的数值
+    // limit_value：极限值
+    // pay_cny_amount扣除余额后微信实际支付的金额
+    const { total, type, token_id, token_amount, limit_value, min_liquidity = 0, pay_cny_amount } = ctx.request.body;
+    const ip = ctx.ips.length > 0 ? ctx.ips[0] !== '127.0.0.1' ? ctx.ips[0] : ctx.ips[1] : ctx.ip;
+    const token = await ctx.service.token.mineToken.get(token_id);
+    if (!token) {
+      ctx.body = ctx.msg.failure;
+      return;
+    }
+    const out_trade_no = ctx.helper.genCharacterNumber(31);
+    const max_tokens = 0;
+    const min_tokens = limit_value;
+    // 创建订单，status为0
+    const createSuccess = await ctx.service.exchange.createOrder({
+      uid: ctx.user.id, // 用户id
+      token_id, // 购买的token id
+      cny_amount: total,
+      pay_cny_amount,
+      token_amount,
+      type: typeOptions[type], // 类型：add，buy_token，sale_token
+      trade_no: out_trade_no, // 订单号
+      openid: '',
+      status: 0, // 状态，0初始，3支付中，6支付成功，9处理完成
+      min_liquidity, // 资金池pool最小流动性，type = add
+      max_tokens, // output为准，最多获得CNY，type = sale_token
+      min_tokens, // input为准时，最少获得Token，type = buy_token
+      recipient: ctx.user.id, // 接收者
+      ip, // ip
+    });
+    // 创建失败直接返回错误
+    if (!createSuccess) {
+      ctx.body = ctx.msg.failure;
+      return;
+    }
+    ctx.body = {
+      ...ctx.msg.success,
+      data: out_trade_no,
+    };
+  }
+  async wxpay() {
+    const { ctx } = this;
+    const { tradeNo, trade_type = 'NATIVE', openid = null } = ctx.request.body;
+    const out_trade_no = tradeNo;
+    const { type, token_id, pay_cny_amount, status } = await ctx.service.exchange.getOrderBytradeNo(tradeNo);
+    // 6 9都代表支付成功 7 8 失败
+    if (status >= 6) {
+      ctx.body = ctx.msg.orderHandled;
+      return;
+    }
+    const { symbol, decimals } = await ctx.service.token.mineToken.get(token_id);
+    const ip = ctx.ips.length > 0 ? ctx.ips[0] !== '127.0.0.1' ? ctx.ips[0] : ctx.ips[1] : ctx.ip;
+    let title = '';
+    if (type === 'buy_token_output' || type === 'buy_token_input') {
+      title = `购买${symbol}`;
+    } else if (title === 'add') {
+      title = '添加流动性';
+    }
+    // 如果订单金额小于0元
+    if (pay_cny_amount <= 0) {
+      ctx.body = {
+        timeStamp: Math.floor(Date.now() / 1000),
+        trade_no: out_trade_no,
+      };
+      return;
+    }
+    // pay_cny_amount扣除余额后微信实际支付的金额
+    const total_fee = Math.floor(pay_cny_amount / Math.pow(10, parseInt(decimals) - 2));
+    let order = {
+      out_trade_no, // 订单号 唯一id商户系统内部订单号，要求32个字符内，只能是数字、大小写字母_-|* 且在同一个商户号下唯一。
+      body: title,
+      total_fee, // 微信最小单位是分
+      spbill_create_ip: ip, // 请求的ip地址
+    };
+    ctx.logger.info('controller wxpay pay params', order);
+    let payargs = {};
+    if (trade_type === 'JSAPI') {
+      order = {
+        ...order,
+        openid,
+      };
+      // 获取微信JSSDK支付参数
+      payargs = await this.app.tenpay.getPayParams(order);
+    } else {
+      order = {
+        ...order,
+        trade_type,
+        product_id: symbol,
+      };
+      // 微信统一下单
+      payargs = await this.app.tenpay.unifiedOrder(order);
+    }
+    ctx.logger.info('controller wxpay pay result', payargs);
+    if (payargs.appId || payargs.appid) {
+      // 更新订单状态为‘支付中’：3
+      await ctx.service.exchange.setStatusPending(order.out_trade_no);
+      ctx.body = {
+        // eslint-disable-next-line no-bitwise
+        timeStamp: '' + (Date.now() / 1000 | 0),
+        ...payargs,
+        trade_no: order.out_trade_no,
+      };
+    } else {
+      ctx.body = ctx.msg.failure;
+    }
+  }
   async pay() {
     const { ctx } = this;
     // total: 输入的cny数值，单位元
@@ -25,7 +135,7 @@ class WxPayController extends Controller {
       ctx.body = ctx.msg.failure;
       return;
     }
-    const out_trade_no = nanoid(31);
+    const out_trade_no = ctx.helper.genCharacterNumber(31);
     const max_tokens = 0;
     const min_tokens = limit_value;
     // 创建订单，status为0
