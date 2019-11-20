bfbb380336dfe57d7cbf031042ae89a22c082161
神樹桜乃 <kodama@sakuno.moe>
2019-11-20 15:42:35 +0800

    It's able to mask email as username

--- app/service/user.js
+++ app/service/user.js
@@ -85,7 +85,7 @@ class UserService extends Service {
     }
 
     const result = {
-      username: user.username,
+      username: this.maskEmailAddress(user.username),
       nickname,
       avatar,
       introduction,
@@ -717,6 +717,40 @@ class UserService extends Service {
     }
 
     return { count, list: posts };
+  }
+
+  maskEmailAddress(str) {
+    // Source: https://html.spec.whatwg.org/multipage/input.html#e-mail-state-(type=email)
+    const regex = /^([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+)(@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)$/;
+    const match = regex.exec(str);
+
+    if (!match)
+      return str;
+
+    let [_, username, rest] = match;
+
+    switch (username.length) {
+      case 1:
+        username = '*';
+        break;
+
+      case 2:
+        username = username[0] + '*';
+        break;
+
+      case 3:
+        username = username[0] + '*' + username[2];
+        break;
+
+      default:
+        const trunkSize = username.length / 4;
+        const firstSize = Math.max(Math.floor(trunkSize), 1);
+        const secondSize = Math.ceil(trunkSize * 2);
+        username = username.slice(0, firstSize) + '*'.repeat(secondSize) + username.slice(firstSize + secondSize);
+        break;
+    }
+
+    return username + rest;
   }
 }
 
