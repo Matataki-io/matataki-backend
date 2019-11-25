1d6613385374a7edba0822e26f0062499e83cfe4
韦海晖 <franciswei98@gmail.com>
2019-11-25 18:54:19 +0800

    feat: newdex bp

--- config/eos.js
+++ config/eos.js
@@ -2,6 +2,7 @@ const blockProducersList = [
   'nodes.get-scatter.com',
   'eos.greymass.com',
   'bp.cryptolions.io',
+  'eos.newdex.one',
   'mainnet.eoscannon.io',
   'api1.eosasia.one',
   'user-api.eoseoul.io',
