d7da5c549d4d882f39bf31ac8c96b3c1cb6fb80a
韦海晖 <franciswei98@gmail.com>
2019-11-25 18:46:24 +0800

    feat: block producer list

---
+++ config/eos.js
@@ -0,0 +1,11 @@
+const blockProducersList = [
+  'nodes.get-scatter.com',
+  'eos.greymass.com',
+  'bp.cryptolions.io',
+  'mainnet.eoscannon.io',
+  'api1.eosasia.one',
+  'user-api.eoseoul.io',
+  'api.eosflare.io',
+];
+
+module.export = blockProducersList;
