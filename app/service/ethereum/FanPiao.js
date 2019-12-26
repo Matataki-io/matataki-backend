d9e471f0f41b9a4af82f8650e9c1af8064715419
韦海晖 <franciswei98@gmail.com>
2019-12-26 12:18:20 +0800

    feat: load gas price from chain

--- app/service/ethereum/FanPiao.js
+++ app/service/ethereum/FanPiao.js
@@ -22,22 +22,23 @@ class FanPiaoService extends Web3Service {
    * @param {number} decimals ERC20 Token Decimals
    * @param {number} initialSupply ERC20 Token 首次发行额度, 单位是 wei
    * 如发行 1145141919810 个饭票 需要填入 1145141919810000000000000000000
-   * @param {address} target 目标的钱包地址
+   * @param {address} issuer 发币者钱包地址
    * @return {Promise<PromiEvent>} send 合约到区块链的交易结果
    */
-  issue(name, symbol, decimals, initialSupply, target) {
-    this.logger.info('FanPiao-20 issuing now:', name, symbol, decimals, initialSupply, target);
+  async issue(name, symbol, decimals, initialSupply, issuer) {
+    this.logger.info('FanPiao-20 issuing now:', name, symbol, decimals, initialSupply, issuer);
+    const gasPrice = await this.web3.eth.getGasPrice();
+    // 我也很无奈，await promise 要等待部署成功或者部署失败（反正都要等到天荒地老）
+    // 要单纯拿 transactionHash 还得用这样的形式
     return new Promise((resolve, reject) => {
       this.initContract().deploy({
         data: contract20_data.bytecode,
-        arguments: [ name, symbol, decimals, initialSupply, target ],
+        arguments: [ name, symbol, decimals, initialSupply, issuer ],
       }).send({ // 发送交易
         from: this.publicKey,
-        gas: 8000000,
-        gasPrice: this.web3.utils.toWei('2', 'gwei'),
+        gas: 5000000,
+        gasPrice,
       })
-        // 我也很无奈，await promise 要等待部署成功或者部署失败（反正都要等到天荒地老）
-        // 要拿 transactionHash 还得用这样的形式
         .on('error', error => { reject(error); })
         .on('transactionHash', txHash => { resolve(txHash); });
     });
