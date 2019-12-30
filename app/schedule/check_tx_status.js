105a59f7a642552c39ad4b6e828ef65a9d5b10fb
韦海晖 <franciswei98@gmail.com>
2019-12-30 18:17:43 +0800

    refactor: rewrite to class

--- app/schedule/check_tx_status.js
+++ app/schedule/check_tx_status.js
@@ -1,8 +1,6 @@
-exports.schedule = {
-  interval: '5m',
-  type: 'all',
-  immediate: true,
-};
+const { Subscription } = require('egg');
+const axios = require('axios');
+
 /**
  * Check Tx Status @author Frank Wei<frank@frankwei.xyz>
  * 功能：这个定时任务会时不时查询数据库 `assets_minetokens_log`表里的交易，
@@ -14,32 +12,45 @@ const txStatusCode = {
   PENDING: 0,
   OK: 1,
 };
-
-exports.task = async function SyncMinetokenTransaction(ctx) {
-  ctx.logger.info('Running SyncMinetokenTransaction at: ', new Date().toLocaleString());
-  const { mysql } = ctx.app;
-  const txsToBeMonitored = await mysql.query(
-    'select * from assets_minetokens_log where on_chain_tx_status=? and tx_hash is not null',
-    [ txStatusCode.PENDING ]
-  );
-  if (!txsToBeMonitored || txsToBeMonitored.length === 0) {
-    return; // 没有 pending 的交易啊，那没事了
+class SyncMinetokenTransaction extends Subscription {
+  static get schedule() {
+    return {
+      interval: '5m',
+      type: 'all',
+      immediate: true,
+    };
   }
-  ctx.logger.info('txs to be monitored: ', txsToBeMonitored);
 
-  // 开始查询交易小票
-  const receipts = await Promise.all(txsToBeMonitored
-    .map(tx => ctx.service.ethereum.web3.getTransactionReceipt(tx.tx_hash))
-  );
+  async subscribe() {
+    this.logger.info('Running SyncMinetokenTransaction at: ', new Date().toLocaleString());
+    const { mysql } = this.ctx.app;
+    const txsToBeMonitored = await mysql.query(
+      'select * from assets_minetokens_log where on_chain_tx_status=? and tx_hash is not null',
+      [ txStatusCode.PENDING ]
+    );
+    if (!txsToBeMonitored || txsToBeMonitored.length === 0) {
+      return; // 没有 pending 的交易啊，那没事了
+    }
+    this.logger.info('txs to be monitored: ', txsToBeMonitored);
 
-  await Promise.all(receipts.map(receipt => {
-    // receipt 为 null （交易未结束）时，直接跳过, 让他继续为 0
-    if (!receipt) { return null; }
-    const on_chain_tx_status = receipt.status ? txStatusCode.OK : txStatusCode.FAIL;
-    return mysql.update('assets_minetokens_log', {
-      on_chain_tx_status,
-    }, {
-      where: { tx_hash: receipt.transactionHash },
-    });
-  }));
-};
+    // 开始查询交易小票
+    const receipts = await Promise.all(txsToBeMonitored
+      .map(tx => this.service.ethereum.web3.getTransactionReceipt(tx.tx_hash))
+    );
+
+    await Promise.all(receipts.map(receipt => {
+      // receipt 为 null （交易未结束）时，直接跳过, 让他继续为 0
+      if (!receipt) { return null; }
+      const on_chain_tx_status = receipt.status ? txStatusCode.OK : txStatusCode.FAIL;
+      return mysql.update('assets_minetokens_log', {
+        on_chain_tx_status,
+      }, {
+        where: { tx_hash: receipt.transactionHash },
+      });
+    }));
+  }
+
+}
+
+
+module.exports = SyncMinetokenTransaction;
