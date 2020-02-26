bfccfde6e0608a9d6f6f55747cc2189310dafd27
韦海晖 <franciswei98@gmail.com>
2020-02-26 11:56:27 +0800

    refactor: refactor on KeepWalletBalance

--- app/schedule/keep_hosting_wallet_balance.js
+++ app/schedule/keep_hosting_wallet_balance.js
@@ -1,6 +1,4 @@
 const { Subscription } = require('egg');
-const axios = require('axios');
-const BigNumber = require('bignumber.js');
 
 class KeepWalletBalance extends Subscription {
   static get schedule() {
@@ -14,35 +12,16 @@ class KeepWalletBalance extends Subscription {
   async subscribe() {
     this.logger.info('Running KeepWalletBalance', new Date().toLocaleString());
     const { web3 } = this.service.ethereum.web3;
-    const { mysql } = this.app;
-    const ethAccounts = await mysql.select('account_hosting', {
-      where: { blockchain: 'ETH' },
-    });
-    const balances = await Promise.all(ethAccounts.map(
-      ({ public_key }) => web3.eth.getBalance(public_key)
-    ));
-    const accountStat = balances.map((_, idx) => ({
-      address: ethAccounts[idx].public_key,
-      balance: balances[idx],
-    }));
-    const lowestLimit = web3.utils.toWei('0.0015', 'ether');
-    const needAirdropList = accountStat.filter(
-      ({ balance }) => new BigNumber(balance).lt(lowestLimit)
-    ).map(({ address }) => address);
-
-    const { data } = await this.requestAirDrop(needAirdropList, Array(needAirdropList.length).fill(web3.utils.toWei('0.002', 'ether')));
-    this.logger.info('Multisend Result', data);
-  }
-
-  async requestAirDrop(targets, amounts) {
-    // AirDrop 工具托管在 Google Firebase Cloud function，是一个 Serverless 应用
-    const { api, token } = this.config.ethereum.airdrop;
-    return axios({
-      url: api,
-      method: 'POST',
-      headers: { 'x-access-token': token },
-      data: { targets, amounts },
-    });
+    const needAirdropList = await this.service.ethereum.etherAirDropperAPI.findNoEtherUser(
+      web3.utils.toWei('0.002', 'ether')
+    );
+    this.logger.info('KeepWalletBalance::needAirdropList', needAirdropList);
+    if (needAirdropList.length !== 0) {
+      const { data } = await this.service.ethereum.etherAirDropperAPI.requestAirDrop(
+        needAirdropList,
+        Array(needAirdropList.length).fill(web3.utils.toWei('0.005', 'ether')));
+      this.logger.info('Multisend Result', data);
+    }
   }
 }
 
