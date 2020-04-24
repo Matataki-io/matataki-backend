290cddfaacc39befb501a0ab9fe0fa62f4f0d53e
shellteo <shellteo@163.com>
2020-04-24 17:13:04 +0800

    update downloader controller

--- app/controller/downloader.js
+++ app/controller/downloader.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 
 class DownloaderController extends Controller {
   async down() {
-    const uid = this.ctx.user.id;
+    // const uid = this.ctx.user.id;
+    const { uid } = this.ctx.params;
     const filePath = await this.service.downloader.promiseZip(uid);
     // const uploadStatus = await this.service.oss.uploadImage(fs.createReadStream(filePath), '');
     // const filePath = path.resolve(this.app.config.static.dir, 'hello.txt');
