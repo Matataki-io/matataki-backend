f867b8d430b351a4c37f870ddeeceeb2d342a003
shellteo <shellteo@163.com>
2020-04-24 15:54:07 +0800

    add downloader

---
+++ app/controller/downloader.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { Controller } = require('egg');
+const fs = require('fs');
+
+class DownloaderController extends Controller {
+  async down() {
+    const uid = this.ctx.user.id;
+    const filePath = await this.service.downloader.promiseZip(uid);
+    // const uploadStatus = await this.service.oss.uploadImage(fs.createReadStream(filePath), '');
+    // const filePath = path.resolve(this.app.config.static.dir, 'hello.txt');
+    this.ctx.attachment(filePath);
+    this.ctx.set('Content-Type', 'application/octet-stream');
+    this.ctx.body = fs.createReadStream(filePath);
+    // fs.unlink(filePath);
+  }
+}
+module.exports = DownloaderController;
