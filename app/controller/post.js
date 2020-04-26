12612d7f4b58757670b6378cd8101cc20ab917b9
Syntacticlosure <1227847322@qq.com>
2020-04-26 14:49:07 +0800

    impr

--- app/controller/post.js
+++ app/controller/post.js
@@ -1122,7 +1122,10 @@ class PostController extends Controller {
         // 是加密的数据，开始解密
         data = JSON.parse(this.service.cryptography.decrypt(data));
       }
-      
+      if(!ctx.query.edit){
+        data.content = await this.service.extmarkdown.transform(data.content,
+          {userId : ctx.user.id}); 
+        }
       ctx.body = ctx.msg.success;
       // 字符串转为json对象
       ctx.body.data = data;
