f7747b89ea4fd372cbbb951c75f0597ba104c343
LittleSound <464388324@qq.com>
2020-06-09 13:49:51 +0800

    【优化】将查询过程的reply和comment合并

--- app/controller/notify.js
+++ app/controller/notify.js
@@ -22,8 +22,8 @@ class NotificationController extends Controller {
     let userIdSet = new Set();
     let postIdSet = new Set();
     let commentIdSet = new Set();
-    let replyIdSet = new Set();
     let announcementIdSet = new Set();
+    // 遍历
     eventList.list.forEach(item => {
       userIdSet.add(item.user_id);
       userIdSet.add(item.min_user_id);
@@ -31,15 +31,16 @@ class NotificationController extends Controller {
       switch(item.object_type) {
         case 'article': // 文章
           postIdSet.add(item.object_id);
-          // 评论
+          // 评论文章
           if(item.action === 'comment') commentIdSet.add(Number(item.remark));
         break;
         case 'user': // 用户
           userIdSet.add(item.object_id);
         break
-        case 'comment':
+        case 'comment': // 评论
           commentIdSet.add(item.object_id);
-          replyIdSet.add(item.remark);
+          // 回复评论
+          if (item.action === 'reply') commentIdSet.add(item.remark);
           break;
         case 'announcement': // 公告
           announcementIdSet.add(item.object_id);
@@ -56,7 +57,6 @@ class NotificationController extends Controller {
       users: userIdSet.size ? await this.service.user.getUserList([...userIdSet], ctx.user.id) : [],
       posts: postIdSet.size ? await this.service.post.getByIdArray([...postIdSet]) : [],
       comments: commentIdSet.size ? await this.service.comment.getByIdArray([...commentIdSet]) : [],
-      reply: replyIdSet.size ? await this.service.comment.getByIdArray([...replyIdSet]) : [],
       announcements: announcementIdSet.size ? await this.service.notify.announcement.getByIdArray([...announcementIdSet]) : []
     };
   }
@@ -91,21 +91,17 @@ class NotificationController extends Controller {
     // 汇总消息中所需信息的数据库索引
     let userIdSet = new Set();
     let commentIdSet = new Set();
-    let replyIdSet = new Set();
     eventList.list.forEach(item => {
       userIdSet.add(item.user_id);
-      if(item.action === 'comment') commentIdSet.add(Number(item.remark));
-      if(item.action === 'reply') replyIdSet.add(Number(item.remark));
+      if (item.action === 'comment' || item.action === 'reply') commentIdSet.add(Number(item.remark));
     })
     // 获取消息中所需的信息
     const users = userIdSet.size ? await this.service.user.getUserList([...userIdSet], ctx.user.id) : [];
     const comments = commentIdSet.size ? await this.service.comment.getByIdArray([...commentIdSet]) : [];
-    const replys = replyIdSet.size ? await this.service.comment.getByIdArray([...replyIdSet]) : [];
     // 拼接数据
     eventList.list.forEach(item => {
       item.user = users.find(user => user.id === item.user_id)
       item.comment = comments.find(comment => comment.id === item.remark)
-      item.reply = replys.find(reply => reply.id === item.remark)
     })
 
     ctx.body = ctx.msg.success;
