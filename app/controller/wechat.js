05e59c10b6761af2ff3e5f1ec182898e887a8910
shellteo <shellteo@163.com>
2020-07-14 11:53:41 +0800

    feat: 添加微信服务号

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -2,6 +2,7 @@
 
 const Controller = require('../core/base_controller');
 const { ApiConfig, ApiConfigKit, WeChat } = require('tnwx');
+const MsgController = require('../wx/MsgController');
 
 class WechatController extends Controller {
   // 获取签名
@@ -41,7 +42,56 @@ class WechatController extends Controller {
   async handleMsg() {
     const { ctx } = this;
     console.log(ctx);
-    ctx.body = 'handleMsg';
+    ctx.logger.info('controller wechat auth: ', ctx.query);
+    const {
+      msgSignature,
+      timestamp,
+      nonce,
+    } = ctx.query;
+    const msgXml = this.ctx.request.rawBody;
+    console.log(msgXml);
+    const appId = this.config.wechat.appId;
+    const appSecret = this.config.wechat.appSecret;
+    const apiConfig = new ApiConfig(appId, appSecret, 'andoromeda');
+    ApiConfigKit.putApiConfig(apiConfig);
+    ApiConfigKit.devMode = true;
+    ApiConfigKit.setCurrentAppId(appId);
+    const msgAdapter = new MsgController();
+    ctx.set('Content-Type', 'text/xml');
+    const msg = await WeChat.handleMsg(msgAdapter, msgXml, msgSignature, timestamp, nonce);
+    console.log(msg);
+    ctx.body = msg;
+
+    // 获取签名相关的参数用于消息解密(测试号以及明文模式无此参数)
+    /* const buffer = [];
+    const req = this.ctx.req;
+    req.on('data', data => {
+      buffer.push(data);
+    });
+
+    req.on('end', () => {
+      const msgXml = Buffer.concat(buffer).toString('utf-8');
+      console.log(msgXml);
+      // 处理消息并响应对应的回复
+      // .
+    }); */
+
+    /* let data = '';
+    this.ctx.req.setEncoding('utf8');
+    this.ctx.req.on('data', chunk => {
+      data += chunk;
+    });
+    const that = this;
+    this.ctx.req.on('end', () => {
+      xml2js(data, { explicitArray: false }, (err, json) => {
+        console.log(json); // 这里的json便是xml转为json的内容
+        that.ctx.body = 'success';
+      });
+    }); */
+
+    /* const xml = await raw(inflate(this.ctx.req));
+    console.log(xml); */
+    // ctx.body = 'handleMsg';
   }
 }
 
---
+++ app/wx/MsgController.js
@@ -0,0 +1,84 @@
+const {
+  MsgAdapter,
+  InFollowEvent,
+  InQrCodeEvent,
+  OutTextMsg,
+  ApiConfigKit,
+  OutNewsMsg,
+  OutCustomMsg,
+} = require('tnwx');
+
+class MsgController extends MsgAdapter {
+  processInTextMsg(inTextMsg) {
+    let outMsg;
+    let content = 'IJPay 让支付触手可及 \n\nhttps://gitee.com/javen205/IJPay';
+    if (inTextMsg.getContent === '极速开发微信公众号') {
+      // 多公众号支持 分别给不同的公众号发送不同的消息
+      if (ApiConfigKit.getApiConfig.getAppId === 'wx614c453e0d1dcd12') {
+        content = '极速开发微信公众号 \n\nhttps://github.com/javen205/weixin_guide';
+        outMsg = new OutTextMsg(inTextMsg);
+        outMsg.setContent(content);
+      } else {
+        content = '极速开发微信公众号 \n\nhttps://github.com/javen205/TNWX';
+        outMsg = new OutTextMsg(inTextMsg);
+        outMsg.setContent(content);
+      }
+    } else if (inTextMsg.getContent === '聚合支付') {
+      // 最新规则：开发者只能回复1条图文消息；其余场景最多可回复8条图文消息
+      outMsg = new OutNewsMsg(inTextMsg);
+      outMsg.addArticle(
+        '聚合支付了解下',
+        'IJPay 让支付触手可及',
+        'https://gitee.com/javen205/IJPay/raw/master/assets/img/IJPay-t.png',
+        'https://gitee.com/javen205/IJPay'
+      );
+      outMsg.addArticle(
+        'jfinal-weixin',
+        '极速开发微信公众号',
+        'https://gitee.com/javen205/IJPay/raw/master/assets/img/IJPay-t.png',
+        'https://gitee.com/JFinal/jfinal-weixin'
+      );
+    } else {
+      // outMsg = new OutTextMsg(inTextMsg);
+      // outMsg.setContent(content);
+      // 转发给多客服PC客户端
+      outMsg = new OutCustomMsg(inTextMsg);
+      console.log('转发给多客服PC客户端');
+    }
+    return outMsg;
+  }
+  processInFollowEvent(inFollowEvent) {
+    if (InFollowEvent.EVENT_INFOLLOW_SUBSCRIBE === inFollowEvent.getEvent) {
+      return this.renderOutTextMsg(
+        inFollowEvent,
+        '感谢你的关注 么么哒 \n\n交流群：114196246'
+      );
+    } else if (
+      InFollowEvent.EVENT_INFOLLOW_UNSUBSCRIBE === inFollowEvent.getEvent
+    ) {
+      console.error('取消关注：' + inFollowEvent.getFromUserName);
+      return this.renderOutTextMsg(inFollowEvent);
+    }
+    return this.renderOutTextMsg(inFollowEvent);
+  }
+
+  processInQrCodeEvent(inQrCodeEvent) {
+    if (InQrCodeEvent.EVENT_INQRCODE_SUBSCRIBE === inQrCodeEvent.getEvent) {
+      console.debug('扫码未关注：' + inQrCodeEvent.getFromUserName);
+      return this.renderOutTextMsg(
+        inQrCodeEvent,
+        '感谢您的关注，二维码内容：' + inQrCodeEvent.getEventKey
+      );
+    } else if (InQrCodeEvent.EVENT_INQRCODE_SCAN === inQrCodeEvent.getEvent) {
+      console.debug('扫码已关注：' + inQrCodeEvent.getFromUserName);
+      return this.renderOutTextMsg(inQrCodeEvent);
+    }
+    return this.renderOutTextMsg(inQrCodeEvent);
+  }
+  renderOutTextMsg(inMsg, content) {
+    const outMsg = new OutTextMsg(inMsg);
+    outMsg.setContent(content ? content : ' ');
+    return outMsg;
+  }
+}
+module.exports = MsgController;
