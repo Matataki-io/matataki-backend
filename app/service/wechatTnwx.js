106a6d2309298ab2ee4eb34b0e4f5a12937303d5
xiaotian <952822399@qq.com>
2020-07-17 20:06:18 +0800

    refactor: 抽离部分if

--- app/service/wechatTnwx.js
+++ app/service/wechatTnwx.js
@@ -63,85 +63,94 @@ class WechatService extends Service {
     const xmlResult = await parser.parseStringPromise(msgXml);
     console.log('xmlResult', xmlResult.xml);
     const msgXmlResult = xmlResult.xml;
+    console.log('msgXmlResult: ', msgXmlResult);
+
+
+    // 扫码登录
+    const scanLogin = async msgXmlResult => {
+      try {
+        // get accesstoken
+        const assessToken = await this.service.wechatApi.getAccessToken();
+        if (!assessToken) return;
+
+        const res = await this.weixinLogin(assessToken, msgXmlResult.FromUserName[0]);
+        console.log('res', res);
+
+        if (res.code === 0) {
+          await this.app.redis.set(`scene:${msgXmlResult.EventKey[0]}`, res.data, 'EX', 60);
+        }
+      } catch (e) {
+        this.logger.error('event SCAN: ', e);
+      }
+    };
+
+    // 扫码关注登录
+    const scanFollowLogin = async (msgXmlResult, eventKey) => {
+      try {
+        // get accesstoken
+        const assessToken = await this.service.wechatApi.getAccessToken();
+        if (!assessToken) return;
+
+        const res = await this.weixinLogin(assessToken, msgXmlResult.FromUserName[0]);
+        console.log('res', res);
+
+        if (res.code === 0) {
+          await this.app.redis.set(`scene:${eventKey}`, res.data, 'EX', 60);
+        }
+      } catch (e) {
+        this.logger.error('event scan subscribe: ', e);
+      }
+    };
 
     // 事件处理
-    if (msgXmlResult.MsgType[0] === 'event') {
+    if (msgXmlResult.MsgType[0] !== 'event') {
+      return msg;
+    }
 
-      if (msgXmlResult.Event[0] === 'SCAN') { // 扫码进入
+    // 判断事件
+    if (msgXmlResult.Event[0] === 'SCAN') { // 扫码进入
 
-        // 如果用户已经关注公众号
-        // SCAN
-        // 478398
+      // 如果用户已经关注公众号
+      // SCAN
+      // 478398
 
-        // 如果有 event key
-        if (msgXmlResult.EventKey[0]) {
-          try {
-            // get accesstoken
-            const assessToken = await this.service.wechatApi.getAccessToken();
-            if (!assessToken) return;
+      // 如果有 event key
+      if (msgXmlResult.EventKey[0]) {
+        scanLogin(msgXmlResult);
+      } else {
+        console.log('not event key, msgXmlResult: ', msgXmlResult);
+      }
 
-            const res = await this.weixinLogin(assessToken, msgXmlResult.FromUserName[0]);
-            console.log('res', res);
+    } else if (msgXmlResult.Event[0] === 'unsubscribe' && msg === 'success') { // 取关了
+      console.log(`${msgXmlResult.FromUserName[0]}取关了`);
+    } else if (msgXmlResult.Event[0] === 'subscribe') { // 关注了
+      console.log(`${msgXmlResult.FromUserName[0]}关注了`);
 
-            if (res.code === 0) {
-              await this.app.redis.set(`scene:${msgXmlResult.EventKey[0]}`, res.data, 'EX', 60);
-            }
-          } catch (e) {
-            this.logger.error('event SCAN: ', e);
-          }
-        } else {
-          console.log('not event key, msgXmlResult: ', msgXmlResult);
-        }
+      // 如果用户还未关注公众号
+      // subscribe
+      // qrscene_478398
 
-      } else if (msgXmlResult.Event[0] === 'unsubscribe' && msg === 'success') { // 取关了
-        console.log(`${msgXmlResult.FromUserName[0]}取关了`);
-      } else if (msgXmlResult.Event[0] === 'subscribe') { // 关注了
-        console.log(`${msgXmlResult.FromUserName[0]}关注了`);
+      // 判断是否有 event key
+      if (msgXmlResult.EventKey[0]) {
 
-        // 如果用户还未关注公众号
-        // subscribe
-        // qrscene_478398
+        const eventKey = msgXmlResult.EventKey[0].split('_');
 
-        // 判断是否有 event key
-        if (msgXmlResult.EventKey[0]) {
-
-          const eventKey = msgXmlResult.EventKey[0].split('_');
-
-          // 判断是否为 qrscene_123456 格式
-          if (eventKey[0] === 'qrscene') { // 认为是扫码关注的
-
-            try {
-              // get accesstoken
-              const assessToken = await this.service.wechatApi.getAccessToken();
-              if (!assessToken) return;
-
-              const res = await this.weixinLogin(assessToken, msgXmlResult.FromUserName[0]);
-              console.log('res', res);
-
-              if (res.code === 0) {
-                await this.app.redis.set(`scene:${eventKey[1]}`, res.data, 'EX', 60);
-              }
-            } catch (e) {
-              this.logger.error('event subscribe: ', e);
-            }
-          } else {
-            // 其他自定义事件
-            console.log('other event key msgXmlResult: ', msgXmlResult);
-          }
-
+        // 判断是否为 qrscene_123456 格式
+        if (eventKey[0] === 'qrscene') { // 认为是扫码关注的
+          scanFollowLogin(msgXmlResult, eventKey[1]);
         } else {
           // 其他自定义事件
-          console.log('not event key, msgXmlResult: ', msgXmlResult);
+          console.log('other event key msgXmlResult: ', msgXmlResult);
         }
 
       } else {
-        // 其他事件
-        console.log('other event, msgXmlResult: ', msgXmlResult);
+        // 没有事件key
+        console.log('not event key, msgXmlResult: ', msgXmlResult);
       }
 
     } else {
-      // 不是事件
-      console.log('not event, msgXmlResult: ', msgXmlResult);
+      // 其他事件
+      console.log('other event, msgXmlResult: ', msgXmlResult);
     }
 
     console.log('msg 45 line ------------------------------');
@@ -218,8 +227,6 @@ class WechatService extends Service {
 
   // 通过wx登录
   async loginByWx(scene) {
-    const { ctx } = this;
-
     if (!scene) return false;
 
     try {
