fbfb27c15864d31f636293639525590d6f3238a2
Frank Wei <frank@frankwei.xyz>
2021-03-15 19:48:14 +0800

    refactor: verify the Captcha sig in middleware

--- app/controller/user.js
+++ app/controller/user.js
@@ -205,19 +205,7 @@ class UserController extends Controller {
   // 将设置用户昵称、个性签名合而为一
   async setProfile() {
     const ctx = this.ctx;
-    const { nickname = null, introduction = null, accept = null, hCaptchaData = null } = ctx.request.body;
-    const hCaptchaKey = this.app.config.hCaptcha.privateKey;
-
-    try {
-      if (!hCaptchaData.token) throw new Error('Bad Captcha');
-      const verifiedCaptchaData = await verify(hCaptchaKey, hCaptchaData.token);
-      if (!verifiedCaptchaData.success) throw new Error('Bad Captcha');
-    } catch (error) {
-      ctx.status = 400;
-      ctx.body = ctx.msg.failure;
-      ctx.body.message = 'bad captcha';
-      return;
-    }
+    const { nickname = null, introduction = null, accept = null } = ctx.request.body;
 
     const setResult = await this.service.user.setProfile(ctx.user.id, nickname, introduction, accept);
     if (setResult === true) {
