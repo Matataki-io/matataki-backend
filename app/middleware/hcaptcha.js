f6a3e5f7f3af125243b555e03f3401ece4974f38
Frank Wei <frank@frankwei.xyz>
2021-03-16 12:35:06 +0800

    feat: if in the list then ignore captcha

--- app/middleware/hcaptcha.js
+++ app/middleware/hcaptcha.js
@@ -3,12 +3,21 @@ const hCaptcha = require('hcaptcha');
 
 module.exports = () => {
   return async function verify(ctx, next) {
-    const hCaptchaKey = ctx.app.config.hCaptcha.privateKey;
+    const { user } = ctx;
+    const { privateKey } = ctx.app.config.hCaptcha;
+    // @todo: fetch it from the db
+    const ingoreUids = [];
+    // 检测前面 middleware 有没有注入用户信息，且是否为可以无视验证码的白名单用户
+    if (user && ingoreUids.indexOf(user.id) > -1) {
+      // 白名单用户，直接无视
+      return next();
+    }
+    // const privateKey = ctx.app.config.hCaptcha.privateKey;
 
     const { hCaptchaData } = ctx.request.body;
     try {
       if (!hCaptchaData.token) throw new Error('Bad Captcha');
-      const verifiedCaptchaData = await hCaptcha.verify(hCaptchaKey, hCaptchaData.token);
+      const verifiedCaptchaData = await hCaptcha.verify(privateKey, hCaptchaData.token);
       if (!verifiedCaptchaData.success) throw new Error('Bad Captcha');
 
       // If no error, then carry on
