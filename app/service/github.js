0c2daf06e563f1d88bdd73c6fd90ad6b53dbccbe
Kumo Lam <gdut7890@outlook.com>
2021-05-18 13:00:55 +0800

    修正username.github.io形式的repo链接问题

--- app/service/github.js
+++ app/service/github.js
@@ -1118,7 +1118,7 @@ ${rawPost}`;
   // 自定义域名暂不考虑
   async judgeSiteLink(username, repo_name) {
     if (repo_name === `${username}.github.io`) {
-      return repo_name;
+      return `https://${repo_name}/`;
     } else {
       return `https://${username}.github.io/${repo_name}/`;
     }
