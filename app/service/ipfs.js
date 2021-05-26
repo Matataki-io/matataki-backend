92661448304be558b4f7cf501892d7c620458cb0
Frank Wei <frank@frankwei.xyz>
2021-05-26 14:59:11 +0800

    refactor: go fleek now, use infura as plan B

--- app/service/ipfs.js
+++ app/service/ipfs.js
@@ -1,23 +1,17 @@
 'use strict';
-const IPFS = require('ipfs-mini');
+// const IPFS = require('ipfs-mini');
 const axios = require('axios').default;
 const FormData = require('form-data');
 const Service = require('egg').Service;
 const { v4 } = require('uuid');
 const fleekStorage = require('@fleekhq/fleek-storage-js');
 
-const IpfsUrl = 'https://ipfs-direct.mttk.net';
+// const IpfsUrl = 'https://ipfs-direct.mttk.net';
 
 class ipfs extends Service {
-  constructor(ctx, app) {
-    super(ctx, app);
-    const { host, port, protocol } = this.config.ipfs_service;
-    this.ipfs = new IPFS({
-      host,
-      port,
-      protocol,
-    });
-  }
+  // constructor(ctx, app) {
+  //   super(ctx, app);
+  // }
 
   async cat(hash) {
     // const data = await Promise.race([
@@ -45,16 +39,7 @@ class ipfs extends Service {
   }
 
   add(data) {
-    return new Promise((resolve, reject) => {
-      this.ipfs.add(data, (err, result) => {
-        if (err) {
-          this.logger.error('ipfs.add error: %j', err);
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
+    return this.uploadToFleek(data);
   }
 
   /**
@@ -64,47 +49,25 @@ class ipfs extends Service {
    */
   async uploadToFleek(file, key = v4()) {
     const fleekConfig = this.config.fleekIPFS;
-    const uploadedFile = await fleekStorage.upload({
-      apiKey: fleekConfig.apiKey,
-      apiSecret: fleekConfig.apiSecret,
-      key: 'mttk_post_' + key,
-      data: file,
-    });
 
-    return uploadedFile.hashV0;
-  }
+    try {
+      const uploadedFile = await fleekStorage.upload({
+        apiKey: fleekConfig.apiKey,
+        apiSecret: fleekConfig.apiSecret,
+        key: 'mttk_post_' + key,
+        data: file,
+      });
 
-  /**
-   * @deprecated
-   * 上传的 AWS 的 IPFS 节点
-   * @param {object} file 文件
-   */
-  async uploadToAws(file) {
-    // const { username, password } = this.config.awsIpfs;
-    // const fd = new FormData();
-    // fd.append('', file);
-    // try {
-    //   const { data } = await axios.post(`${IpfsUrl}/api/v0/add`, fd, {
-    //     auth: { username, password },
-    //     headers: fd.getHeaders(),
-    //     params: { pin: "true" },
-    //     timeout: 1000 * 10
-    //   });
-    //   return data.Hash;
-    // } catch (error) {
-    //   if (error.message.indexOf('timeout') > -1) {
-    //     this.logger.error('uploadToAws failed', 'server down, retry with public node');
-    //     await this.service.system.notification.pushTextToDingTalk(
-    //       "ipfs", 
-    //       `（👷IPFS系统警告）监测到 ${IpfsUrl} 的上传接口无法访问，请工程师登录 AWS 检查 EC2 东京节点的健康状态，如果无响应请重启机器。 (AWS: https://aws.amazon.com/)`
-    //     );
-    //     return this.uploadToPublic(file);
-    //   } else {
-    //     this.logger.error('uploadToAws failed', error);
-    //     throw error
-    //   }
-    // }
-    return this.uploadToPublic(file);
+      return uploadedFile.hashV0;
+    } catch (error) {
+      this.logger.error('Upload to fleek have error: ', error);
+      await this.service.system.notification.pushTextToDingTalk(
+        'ipfs', 
+        '(👷IPFS系统警告) 监测到 Fleek.Co 的 IPFS 存储上传接口无法访问，请工程师登录 app.fleek.co 检查状态。现在用 Infura 公共节点顶替。'
+      );
+      const hashWithInfura = await this.uploadToPublic(file);
+      return hashWithInfura;
+    }
   }
 
   /**
